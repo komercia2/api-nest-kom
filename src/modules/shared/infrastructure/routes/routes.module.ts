497fbde24aa9f15f51eab2f990e9fd442dd09ab1
Daniel Quintero <quinterod164@gmail.com>
2023-07-15 21:17:29 -0500

    feat: add routes modules

---
+++ src/modules/shared/infrastructure/routes/routes.module.ts
@@ -0,0 +1,32 @@
+import { GlobalModule } from "@global/global.module"
+import { Module } from "@nestjs/common"
+import { RouterModule, RouteTree } from "@nestjs/core"
+import { AppModule } from "src/app.module"
+
+const apiVersions = { v1: "v1" }
+const prefix = "api"
+
+/**
+ * @name routes
+ * @description Routes of the application to be registered in the application module
+ * @returns RouteTree[]
+ */
+const routes: RouteTree[] = [
+	{
+		path: "/",
+		module: AppModule
+	},
+	{
+		path: `${prefix}/${apiVersions.v1}/common`,
+		module: GlobalModule
+	}
+]
+
+/**
+ * @name RoutesModule
+ * @description Module to register all routes of the application
+ */
+@Module({
+	imports: [RouterModule.register(routes)]
+})
+export class RoutesModule {}
