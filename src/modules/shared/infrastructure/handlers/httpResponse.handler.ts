6a961473364a6b7403b6f546773d98d65d3fe26e
Daniel Quintero <quinterod164@gmail.com>
2023-07-15 21:02:07 -0500

    feat: add http response handler

---
+++ src/modules/shared/infrastructure/handlers/httpResponse.handler.ts
@@ -0,0 +1,27 @@
+import { Response } from "express"
+
+/**
+ * @name HttpResponseType
+ * @description Type for the http response handler function
+ */
+type HttpResponseType = {
+	data: unknown
+	message: string
+	success: boolean
+	statusCode: number
+}
+
+/**
+ * @name handlerHttpResponse
+ * @param res  Response object
+ * @param data Data to be sent in the response
+ * @description Function to handle the http response
+ */
+export const handlerHttpResponse = (res: Response, data: HttpResponseType) => {
+	const { statusCode, success, message, data: responseData } = data
+	res.status(statusCode).send({
+		success,
+		message,
+		data: responseData
+	})
+}
