6ee3264b347828f23add3d619c6c6115c7266f2d
Daniel Quintero <quinterod164@gmail.com>
2023-07-18 16:24:33 -0500

    feat: :bookmark: update swagger version tag

--- src/modules/shared/infrastructure/docs/swagger.config.ts
+++ src/modules/shared/infrastructure/docs/swagger.config.ts
@@ -10,7 +10,7 @@ export const getSwaggerConfig = () => {
 	const swaggerConfig = new DocumentBuilder()
 		.setTitle("Komercia NestJS API")
 		.setDescription("This is the API documentation for the Komercia NestJS API")
-		.setVersion("1.0")
+		.setVersion("1.0.0")
 		.build()
 	return {
 		path: path,
