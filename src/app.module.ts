c1d65ce46c61b8186b8c08d2e301bf5fca96692e
Daniel Quintero <quinterod164@gmail.com>
2023-07-18 16:27:47 -0500

    feat: add config module, typeorm async config

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,22 +2,31 @@ import { classes } from "@automapper/classes"
 import { AutomapperModule } from "@automapper/nestjs"
 import { GlobalModule } from "@global/global.module"
 import { Module } from "@nestjs/common"
+import { ConfigModule } from "@nestjs/config"
 import { TypeOrmModule } from "@nestjs/typeorm"
 import { pinoConfig } from "@shared/infrastructure/configs/logs"
+import { TypeOrmService } from "@shared/infrastructure/database/typeorm"
 import { RoutesModule } from "@shared/infrastructure/routes"
 import { LoggerModule } from "nestjs-pino"
 
 import { AppController } from "./app.controller"
-import { typeOrmConfig } from "./typeorm.config"
 
 @Module({
 	imports: [
 		LoggerModule.forRoot(pinoConfig),
-		AutomapperModule.forRoot({ strategyInitializer: classes() }),
-		TypeOrmModule.forRoot(typeOrmConfig),
+		ConfigModule.forRoot({
+			envFilePath: [".env.development", ".env.production"],
+			isGlobal: true
+		}),
+		// AutomapperModule.forRoot({ strategyInitializer: classes() }),
+		TypeOrmModule.forRootAsync({
+			useClass: TypeOrmService,
+			inject: [ConfigModule]
+		}),
 		RoutesModule,
 		GlobalModule
 	],
+	providers: [ConfigModule],
 	controllers: [AppController]
 })
 export class AppModule {}
