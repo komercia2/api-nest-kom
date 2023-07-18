8e6437dbdb6d5c9623f8c25220849b23ac85ffcf
Daniel Quintero <quinterod164@gmail.com>
2023-07-18 16:29:18 -0500

    feat: add global prefix

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import { RequestMethod } from "@nestjs/common"
+import { ConfigService } from "@nestjs/config"
 import { NestFactory } from "@nestjs/core"
 import { SwaggerModule } from "@nestjs/swagger"
 import { getSwaggerConfig } from "@shared/infrastructure/docs"
@@ -7,19 +9,24 @@ import { Logger } from "nestjs-pino"
 import { AppModule } from "./app.module"
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule, { bufferLogs: true })
+	const app = (await NestFactory.create(AppModule, { bufferLogs: true })).setGlobalPrefix("api}", {
+		exclude: [{ path: "/", method: RequestMethod.GET }]
+	})
+
+	const configService = app.get(ConfigService)
 	const { config, path } = getSwaggerConfig()
 	const document = SwaggerModule.createDocument(app, config)
 
 	SwaggerModule.setup(path, app, document, {
 		customSiteTitle: "Komercia API Docs",
-		swaggerOptions: { docExpansion: "none" }
+		swaggerOptions: { docExpansion: "none" },
+		useGlobalPrefix: true
 	})
 
 	app.useLogger(app.get(Logger))
 	app.use(compression())
 
-	await app.listen(process.env.PORT || 3000)
+	await app.listen(configService.get("PORT") || 3000)
 }
 
 bootstrap()
