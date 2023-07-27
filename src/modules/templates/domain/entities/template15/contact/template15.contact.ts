6ffb01eeb57c690c3353acbe0cb8d8de0888e017
Daniel Quintero <quinterod164@gmail.com>
2023-07-27 10:47:48 -0500

    fix(templates): :pencil2: add missing fields

--- src/modules/templates/domain/entities/template15/contact/template15.contact.ts
+++ src/modules/templates/domain/entities/template15/contact/template15.contact.ts
@@ -26,6 +26,8 @@ export class Template15Contact {
 		color_text = contactSettings.color_text,
 		color_icon = contactSettings.color_icon,
 		color_title_form = contactSettings.color_title_form,
+		background_color_2 = contactSettings["--background_color_2"],
+		color_input = contactSettings.color_input,
 		color_text_input = contactSettings.color_text_input,
 		color_btn_form = contactSettings.color_btn_form,
 		color_text_btn_form = contactSettings.color_text_btn_form
@@ -39,6 +41,8 @@ export class Template15Contact {
 		this.color_text = color_text
 		this.color_icon = color_icon
 		this.color_title_form = color_title_form
+		this["--background_color_2"] = background_color_2
+		this.color_input = color_input
 		this.color_text_input = color_text_input
 		this.color_btn_form = color_btn_form
 		this.color_text_btn_form = color_text_btn_form
