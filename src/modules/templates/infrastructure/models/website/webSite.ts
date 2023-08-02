0b54867e4c2a623c0f04e26a93d5b3f8fc0ac469
Daniel Quintero <quinterod164@gmail.com>
2023-08-02 10:04:40 -0500

    feat(templates): :label: add website mongoose model

---
+++ src/modules/templates/infrastructure/models/website/webSite.ts
@@ -0,0 +1,43 @@
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
+import { Transform } from "class-transformer"
+import { ObjectId, Types } from "mongoose"
+
+import { Template15Model } from "../template15"
+
+@Schema({ collection: "websites" })
+export class WebSiteModel {
+	@Transform(({ value }) => value.toString())
+	_id: ObjectId
+
+	@Prop({ required: true, type: Number })
+	storeId: number
+
+	@Prop({ required: true, type: String, unique: true })
+	subdomain: string
+
+	@Prop({ required: true, type: Number })
+	templateNumber: number
+
+	@Prop({ required: false, type: String, unique: true })
+	domain: string
+
+	@Prop({ required: true, type: Boolean, unique: true })
+	isMain: boolean
+
+	@Prop({ required: true, type: Boolean })
+	active: boolean
+
+	@Prop({ required: false, type: Date })
+	createdAt: Date
+
+	@Prop({ required: false, type: Date })
+	updatedAt: Date | null
+
+	@Prop({ required: false, type: Date })
+	deletedAt: Date | null
+
+	@Prop({ required: false, type: Types.ObjectId, ref: Template15Model.name })
+	templateId: Types.ObjectId
+}
+
+export const WebsitesSchema = SchemaFactory.createForClass(WebSiteModel)
