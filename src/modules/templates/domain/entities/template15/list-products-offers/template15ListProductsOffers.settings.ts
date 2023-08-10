fb33cc0a847ab36c80ba6f25f8476750041f17aa
Daniel Quintero <quinterod164@gmail.com>
2023-08-10 09:20:01 -0500

    fix(templates): :pencil2: fix template15 listProductsOffers settings

--- src/modules/templates/domain/entities/template15/list-products-offers/template15ListProductsOffers.settings.ts
+++ src/modules/templates/domain/entities/template15/list-products-offers/template15ListProductsOffers.settings.ts
@@ -1,9 +1,7 @@
 export const listProductsOffersSettings = {
 	"--background_color_1": "#FFFFFF",
-	visible: "VERDADERO",
+	visible: true,
 	title: "Recommended For You",
 	color_title_1: "#ed6436",
-	color_title_2: "#1f2021",
-	fontSizeTitle: "40px",
 	fontWeighTitle: "600"
 }
