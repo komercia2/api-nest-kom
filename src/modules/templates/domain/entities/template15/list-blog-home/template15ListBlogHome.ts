6aa4545d8fd1b9ba37e8bda7d8499b7f34d68328
Daniel Quintero <quinterod164@gmail.com>
2023-08-10 09:50:07 -0500

    fix(templates): :pencil2: fix template 15 listBlogHome settings

--- src/modules/templates/domain/entities/template15/list-blog-home/template15ListBlogHome.ts
+++ src/modules/templates/domain/entities/template15/list-blog-home/template15ListBlogHome.ts
@@ -5,25 +5,19 @@ export class Template15ListBlogHome {
 	visible: boolean
 	title: string
 	color_title_1: string
-	color_title_2: string
 	fontWeightTitle: string
-	fontSizeTitle: string
 
 	constructor(
 		background_color_1 = listBlogHomeSettings["--background_color_1"],
 		visible = listBlogHomeSettings.visible,
 		title = listBlogHomeSettings.title,
 		color_title_1 = listBlogHomeSettings.color_title_1,
-		color_title_2 = listBlogHomeSettings.color_title_2,
-		fontWeightTitle = listBlogHomeSettings.fontWeightTitle,
-		fontSizeTitle = listBlogHomeSettings.fontSizeTitle
+		fontWeightTitle = listBlogHomeSettings.fontWeightTitle
 	) {
 		this["--background_color_1"] = background_color_1
 		this.visible = visible
 		this.title = title
 		this.color_title_1 = color_title_1
-		this.color_title_2 = color_title_2
-		this.fontSizeTitle = fontSizeTitle
 		this.fontWeightTitle = fontWeightTitle
 	}
 }
