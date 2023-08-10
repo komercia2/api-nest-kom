94e0556c3b21930e4d362d2681eebb6e290ad31a
Daniel Quintero <quinterod164@gmail.com>
2023-08-10 09:25:12 -0500

    fix(templates): :pencil2: fix template 15 information default settings

--- src/modules/templates/domain/entities/template15/information/template15Information.settings.ts
+++ src/modules/templates/domain/entities/template15/information/template15Information.settings.ts
@@ -1,6 +1,7 @@
 export const informationSettings = {
 	"--background_color_1": "#ed6436",
 	"--background_color_2": "#E7E6E4",
+	visible: true,
 	img: "https://res.cloudinary.com/komerciaacademico/image/upload/v1689700301/template15/information_jygwp8.jpg",
 	icon: "https://res.cloudinary.com/komerciaacademico/image/upload/v1689700302/template15/logoInformation_sdnrdx.png",
 	text: "Save big annually with our veterinarian recommended wellness plan.",
@@ -10,5 +11,5 @@ export const informationSettings = {
 	color_text_btn: "#FFFFFF",
 	color_bg_btn: "#000000",
 	url_redirect: "/productos",
-	visible_btn: "VERDADERO"
+	visible_btn: true
 }
