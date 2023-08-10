2efda704b4e8d566f9cf0ea5c3d19cd2309a81fc
Daniel Quintero <quinterod164@gmail.com>
2023-08-10 09:11:53 -0500

    fix(templates): :pencil2: fix template 15 listproductshome visible field as boolean

--- src/modules/templates/domain/entities/template15/list-products-home/template15ListProductHome.settings.ts
+++ src/modules/templates/domain/entities/template15/list-products-home/template15ListProductHome.settings.ts
@@ -1,6 +1,6 @@
 export const listProductHomeSettings = {
 	"--background_color_1": "#FFFFFF",
-	visible: "VERDADERO",
+	visible: true,
 	title: "Best Seller",
 	color_title_1: "#ed6436",
 	color_title_2: "#1f2021",
