ba30c2311cf96579ecbc63858bc34cc2e4b6cf77
Daniel Quintero <quinterod164@gmail.com>
2023-08-12 15:48:58 -0500

    feat(templates): :sparkles: add website creator service use case

--- src/modules/templates/infrastructure/services/websiteMongoose.service.ts
+++ src/modules/templates/infrastructure/services/websiteMongoose.service.ts
@@ -1,22 +1,88 @@
-import { Injectable } from "@nestjs/common"
+import { Inject, Injectable } from "@nestjs/common"
 import { InjectModel } from "@nestjs/mongoose"
 import { DatabaseTransactionErrorException } from "@shared/infrastructure/exceptions"
+import { TemplateNotFoundException } from "@templates/application/exceptions"
+import { Template15 } from "@templates/domain/entities/template15"
 import { WebSiteEntity, WebSiteEntityProps } from "@templates/domain/entities/websites"
-import { Model } from "mongoose"
+import { WebSiteTemplate } from "@templates/domain/entities/websites/webSiteTemplate"
+import { isValidObjectId, Model, ObjectId } from "mongoose"
 
+import { InfrastructureInjectionTokens } from "../infrastructure-injection.tokens"
 import { WebSiteModel } from "../models/website"
+import { createObjectIdFromHexString } from "../util"
+import { Template15MongoService } from "./template15Mongoose.service"
 
 @Injectable()
 export class WebsiteMongooseService {
-	constructor(@InjectModel(WebSiteModel.name) private readonly websiteModel: Model<WebSiteModel>) {}
+	private readonly validServices = new Map([[15, this.template15MongoService]])
+
+	constructor(
+		@InjectModel(WebSiteModel.name) private readonly websiteModel: Model<WebSiteModel>,
+
+		@Inject(InfrastructureInjectionTokens.Template15MongoService)
+		private readonly template15MongoService: Template15MongoService
+	) {}
 
 	create = async (data: WebSiteEntityProps) => {
 		try {
-			const websiteCreated = await new this.websiteModel(data).save()
-			return !!websiteCreated._id
+			const { templateNumber } = data
+			let websiteCreated
+
+			const repository = this.getRepositoryByTemplateNumber(templateNumber)
+
+			if (!repository) {
+				websiteCreated = await this.saveWebSite(data, null)
+			} else {
+				const { _id } = await repository.create2()
+				websiteCreated = await this.saveWebSite(data, _id)
+			}
+
+			return !!websiteCreated?._id
 		} catch (error) {
 			throw new DatabaseTransactionErrorException("Has been an error creating the website")
 		}
+	}
+
+	getWebSite = async (templateId: string): Promise<WebSiteEntity> => {
+		try {
+			const isValidTemplateId = isValidObjectId(templateId)
+			const parsedTemplateId = isValidTemplateId ? createObjectIdFromHexString(templateId) : null
+
+			const getWebSiteDataOperation = this.websiteModel
+				.findOne({ templateId: parsedTemplateId })
+				.exec()
+			const getTemplateOperation = this.template15MongoService.find(templateId)
+
+			const results = await Promise.allSettled([getWebSiteDataOperation, getTemplateOperation])
+
+			const [webSiteDataResult, templateResult] = results
+
+			if (webSiteDataResult.status === "rejected") throw new Error("WebSite not found")
+
+			const webSiteData = webSiteDataResult.value
+
+			if (!webSiteData) throw new Error("WebSite not found")
+
+			const template = templateResult.status === "fulfilled" ? templateResult.value : null
+
+			return this.fromModelToEntity(webSiteData.toObject(), template)
+		} catch (error) {
+			throw new DatabaseTransactionErrorException(
+				"Has been an error getting the website and template"
+			)
+		}
+	}
+
+	private saveWebSite = async (data: WebSiteEntityProps, templateId: ObjectId | null) => {
+		return await new this.websiteModel({
+			...data,
+			templateId
+		}).save()
+	}
+
+	private getRepositoryByTemplateNumber = (templateNumber: number) => {
+		const repository = this.validServices.get(templateNumber)
+		return repository
 	}
 
 	verifyDomainAvailability = async (domain: string) => {
@@ -46,7 +112,6 @@ export class WebsiteMongooseService {
 			const websites = await this.websiteModel.find({ storeId })
 			return websites.map((website) => this.fromModelToEntity(website.toObject()))
 		} catch (error) {
-			console.log(error)
 			throw new DatabaseTransactionErrorException("Has been an error getting the websites")
 		}
 	}
@@ -62,7 +127,25 @@ export class WebsiteMongooseService {
 		}
 	}
 
-	fromModelToEntity = (website: WebSiteModel) => {
+	findTemplateIdByCriteria = async (criteria: string) => {
+		try {
+			const searchQuery = [{ domain: criteria }, { subdomain: criteria }]
+			const template = await this.websiteModel.findOne({ $or: searchQuery, active: true }).exec()
+
+			if (!template)
+				throw new TemplateNotFoundException(`Template not found for criteria ${criteria}`)
+
+			if (!template?.templateId) return null
+
+			return template.templateId.toString()
+		} catch (error) {
+			throw new DatabaseTransactionErrorException(
+				"There was an error finding the template id by criteria"
+			)
+		}
+	}
+
+	fromModelToEntity = (website: WebSiteModel, template?: WebSiteTemplate | null) => {
 		return new WebSiteEntity({
 			_id: String(website._id),
 			domain: website.domain,
@@ -70,7 +153,9 @@ export class WebsiteMongooseService {
 			storeId: website.storeId,
 			active: website.active,
 			isMain: website.isMain,
-			templateNumber: website.templateNumber
+			templateId: website.templateId?.toString() ?? null,
+			templateNumber: website.templateNumber,
+			webSiteTemplate: template
 		})
 	}
 }
