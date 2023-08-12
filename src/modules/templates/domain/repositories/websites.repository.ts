30fb6308a66beebb8221d9a5cf6ebcbea5a7a0d9
Daniel Quintero <quinterod164@gmail.com>
2023-08-12 16:56:14 -0500

    refactor(templates): :recycle: update id into props obj

--- src/modules/templates/domain/repositories/websites.repository.ts
+++ src/modules/templates/domain/repositories/websites.repository.ts
@@ -8,8 +8,9 @@ export interface IWebSitesRepository {
 	findTemplateIdByCriteria(criteria: string): Promise<string | null>
 	getWebSite(target: string | null): Promise<WebSiteEntity | null>
 	getWebSiteListByStoreId(storeId: number): Promise<WebSiteEntity[]>
-	updateWebsiteInfo(
-		_id: string,
-		props: { isMain: boolean; active: boolean }
-	): Promise<WebSiteEntity>
+	updateWebsiteInfo(props: {
+		_id: string
+		isMain: boolean
+		active: boolean
+	}): Promise<WebSiteEntity>
 }
