ca63f5f593d548e889c310fdabef738de8acc42c
Daniel Quintero <quinterod164@gmail.com>
2023-08-12 17:28:09 -0500

    fix(templates): :pencil2: fix dto required fields

--- src/modules/templates/application/command/dtos/updateWebSiteDto.ts
+++ src/modules/templates/application/command/dtos/updateWebSiteDto.ts
@@ -1,19 +1,9 @@
-import { IsBoolean, IsNotEmpty, IsString } from "class-validator"
-
 export class UpdateWebSiteDto {
-	@IsString()
-	@IsNotEmpty()
-	_id: string
-
-	@IsString()
 	subdomain: string
 
-	@IsString()
 	domain: string
 
-	@IsBoolean()
 	isMain: boolean
 
-	@IsBoolean()
 	active: boolean
 }
