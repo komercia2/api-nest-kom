f381af79226c13eed28810931953c14fd21720d8
Daniel Quintero <quinterod164@gmail.com>
2023-10-26 01:36:41 -0500

    refactor: :truck: update common module location

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -3,12 +3,12 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { PublicApiKeyAuthMiddleware } from "@shared/infrastructure/middlewares/keys"
 import { Ciudades } from "src/entities"
 
-import { CommonApplicationInjectionTokens } from "../application/common-application-injection-token"
-import { GetAllCitiesQuery } from "../application/query"
-import { CommonInfrastructureInjectionTokens } from "./common-infrastructure-injection-tokens"
-import { CityController } from "./controllers"
-import { MySQLCityRepository } from "./repositories"
-import { MySQLCityService } from "./services"
+import { CommonApplicationInjectionTokens } from "./application/common-application-injection-token"
+import { GetAllCitiesQuery } from "./application/query"
+import { CommonInfrastructureInjectionTokens } from "./infrastructure/common-infrastructure-injection-tokens"
+import { CityController } from "./infrastructure/controllers"
+import { MySQLCityRepository } from "./infrastructure/repositories"
+import { MySQLCityService } from "./infrastructure/services"
 
 const application = [
 	{
