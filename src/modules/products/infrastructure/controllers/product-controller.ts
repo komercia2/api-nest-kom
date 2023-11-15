9ae69b31fe54e9ec60475179109652be8e22416c
Daniel Quintero <quinterod164@gmail.com>
2023-11-14 22:48:44 -0500

    fix: update product-controller.ts to return NO_CONTENT status code when no products are found

--- src/modules/products/infrastructure/controllers/product-controller.ts
+++ src/modules/products/infrastructure/controllers/product-controller.ts
@@ -61,7 +61,7 @@ export class ProductController {
 				handlerHttpResponse(res, {
 					data: null,
 					message: "No products found",
-					statusCode: HttpStatus.NOT_FOUND,
+					statusCode: HttpStatus.NO_CONTENT,
 					success: true
 				})
 				return
