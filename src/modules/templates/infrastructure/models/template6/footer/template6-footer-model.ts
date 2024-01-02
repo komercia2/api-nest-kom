46f0065caf525ce0c925909895f734ddcde3b904
Daniel Quintero <quinterod164@gmail.com>
2024-01-02 09:53:21 -0500

    feat(templates): :pencil2: update template6 footer fields

--- src/modules/templates/infrastructure/models/template6/footer/template6-footer-model.ts
+++ src/modules/templates/infrastructure/models/template6/footer/template6-footer-model.ts
@@ -3,6 +3,9 @@ import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose"
 @Schema({ _id: false })
 export class Template6FooterModel {
 	@Prop({ required: true })
+	bg_color: string
+
+	@Prop({ required: true })
 	"--background_color_1": string
 
 	@Prop({ required: true })
@@ -10,9 +13,6 @@ export class Template6FooterModel {
 
 	@Prop({ required: true })
 	"--color_icon": string
-
-	@Prop({ required: true })
-	"--color_title": string
 
 	@Prop({ required: true })
 	"--color_text": string
