75f39d276ede6f86bd7d32900a39b29dcfe25bfc
Daniel Quintero <quinterod164@gmail.com>
2024-01-02 08:58:57 -0500

    feat(templates): :sparkles: update template 6 header values

--- src/modules/templates/domain/entities/template6/header/template6-banner-settings.ts
+++ src/modules/templates/domain/entities/template6/header/template6-banner-settings.ts
@@ -1,8 +1,8 @@
 export const template6HeaderSettings = {
 	"--background_color_1": "#FFFFFF",
-	"--color_text": "#222222",
-	"--color_icon": "#222222",
-	"--color_border": "#222222",
+	"--color_text": "#6b7280",
+	"--color_icon": "#111827",
+	"--color_border": "#e5e7eb",
 	"--padding_logo": "0px",
 	"--with_logo": "120px"
 }
