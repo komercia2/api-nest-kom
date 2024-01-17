96bc462e6d62b265748e1508fcb323fec0f23be0
Daniel Quintero <quinterod164@gmail.com>
2024-01-17 13:46:42 -0500

    feat: :lock: add public key middleware

--- src/modules/mails/mails.module.ts
+++ src/modules/mails/mails.module.ts
@@ -1,5 +1,6 @@
-import { Module } from "@nestjs/common"
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common"
 import { MailService } from "@sendgrid/mail"
+import { PublicApiKeyAuthMiddleware } from "@shared/infrastructure/middlewares/keys"
 
 import { MailsController } from "./mails.controller"
 import { MailsService } from "./mails.service"
@@ -8,4 +9,8 @@ import { MailsService } from "./mails.service"
 	controllers: [MailsController],
 	providers: [MailsService, MailService]
 })
-export class MailsModule {}
+export class MailsModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(PublicApiKeyAuthMiddleware).forRoutes(MailsController)
+	}
+}
