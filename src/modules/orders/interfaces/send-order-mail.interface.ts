54cb2b2dbe3f18d26c5fe164661be640ebcc2214
Daniel Quintero <quinterod164@gmail.com>
2024-01-26 14:54:14 -0500

    feat: :sparkles: add email sender for orders

---
+++ src/modules/orders/interfaces/send-order-mail.interface.ts
@@ -0,0 +1,46 @@
+export interface OrderEmailDto {
+	total: string
+	logo: string
+	logoKomercia: string
+	logoKomerciaWhite: string
+	isClient: boolean
+	IdOrden: number
+	data: Data
+}
+
+export interface Data {
+	venta: Venta
+}
+
+export interface Venta {
+	usuario: Usuario
+	fecha: string
+	total: number
+	direccion_entrega: DireccionEntrega
+	costo_envio: string
+	descuento: number
+	method_shipping: string
+	tienda_venta: TiendaVenta
+}
+
+export interface Usuario {
+	nombre: string
+	tipo_identificacion: string
+	identificacion: string
+}
+
+export interface DireccionEntrega {
+	direccion: string
+	tag: string
+	nombre: string
+	celular: string
+	barrio: string
+}
+
+export interface TiendaVenta {
+	nombre: string
+	subdominio: string
+	dominio: string
+	email_tienda: string
+	telefono: string
+}
--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -9,24 +9,32 @@ import {
 	ProductosInfo,
 	ProductosVariantes,
 	ProductosVariantesCombinaciones,
+	Tiendas,
+	TiendasInfo,
 	Users
 } from "src/entities"
+import { StoreNotification } from "src/entities/StoreNotifications"
 
+import { MailsModule } from "../mails/mails.module"
 import { OrdersController } from "./orders.controller"
 import { OrdersService } from "./orders.service"
 
 @Module({
 	imports: [
+		MailsModule,
 		TypeOrmModule.forFeature([
 			Carritos,
 			Productos,
 			ProductosVariantes,
 			ProductosVariantesCombinaciones,
 			ProductosInfo,
+			Tiendas,
 			ProductosCarritos,
+			TiendasInfo,
 			MensajeOrden,
 			Users,
-			Clientes
+			Clientes,
+			StoreNotification
 		])
 	],
 	controllers: [OrdersController],
