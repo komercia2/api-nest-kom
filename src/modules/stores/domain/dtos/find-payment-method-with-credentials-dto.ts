ed1ac9750ef891e452bd5ece2a6d9ef5eed0ca2d
Daniel Quintero <quinterod164@gmail.com>
2024-02-08 17:04:46 -0500

    fix: :bug: update payments gateways enum as array [client error]

--- src/modules/stores/domain/dtos/find-payment-method-with-credentials-dto.ts
+++ src/modules/stores/domain/dtos/find-payment-method-with-credentials-dto.ts
@@ -1,8 +1,19 @@
-import { IsEnum } from "class-validator"
+import { IsEnum, IsIn } from "class-validator"
 
 import { StorePaymentGateawayMethods } from "../enums/store-payment-gateaway-methods"
 
 export class FindPaymentMethodWithCredentialsDto {
-	@IsEnum(StorePaymentGateawayMethods)
+	@IsIn([
+		"EPAYCO",
+		"PAYU",
+		"CREDIBANCO",
+		"WOMPI",
+		"PAYMENTS_WAY",
+		"WEPAY4U",
+		"TU_COMPRA",
+		"FLOW",
+		"MERCADOPAGO",
+		"ADDI"
+	])
 	public readonly paymentGateawayMethod: StorePaymentGateawayMethods
 }
