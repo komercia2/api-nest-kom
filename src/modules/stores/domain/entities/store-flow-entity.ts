ed9f8cd1f1f5f75cd5c47f7eadfe1aa5aa13eca2
Daniel Quintero <quinterod164@gmail.com>
2024-03-11 11:29:37 -0500

    fix: :pencil2: update secret key field in flow payment gateway

--- src/modules/stores/domain/entities/store-flow-entity.ts
+++ src/modules/stores/domain/entities/store-flow-entity.ts
@@ -1,7 +1,7 @@
 export class StoreFlowEntity {
-	constructor(readonly estado: boolean, readonly api_key: string, readonly secret_key: string) {
+	constructor(readonly estado: boolean, readonly api_key: string, readonly secretKey: string) {
 		this.estado = estado
 		this.api_key = api_key
-		this.secret_key = secret_key
+		this.secretKey = secretKey
 	}
 }
