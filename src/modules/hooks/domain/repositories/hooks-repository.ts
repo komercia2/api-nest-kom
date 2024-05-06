e703295c732a9c7070b333b34cfe03ec4b42f054
DQuintero15 <quinterod164@gmail.com>
2024-05-06 10:35:18 -0500

    feat: add processWompiPaymentStatus method to IHookRepository

--- src/modules/hooks/domain/repositories/hooks-repository.ts
+++ src/modules/hooks/domain/repositories/hooks-repository.ts
@@ -1,6 +1,7 @@
-import { AddiHookEntity, OrderHookEntity } from "../entities"
+import { AddiHookEntity, OrderHookEntity, WompiEntity } from "../entities"
 
 export interface IHookRepository {
 	notifyOrderCreated(order: OrderHookEntity): Promise<void>
 	proccessAddiApplicationStatus(order: AddiHookEntity): Promise<AddiHookEntity>
+	processWompiPaymentStatus(order: WompiEntity): Promise<void>
 }
