5063cd571ad70e09bdca80f33eb195286f6cf362
DQuintero15 <quinterod164@gmail.com>
2024-06-06 11:40:26 -0500

    feat: add SaveClickedPayCartCommand to handle saving clicked pay cart event

--- src/modules/stores/application/command/index.ts
+++ src/modules/stores/application/command/index.ts
@@ -1,3 +1,4 @@
 export * from "./change-payment-gateway-status-command"
 export * from "./create-payment-gateway-command"
+export * from "./save-clicked-pay-cart.command"
 export * from "./save-store-analytic-command"
