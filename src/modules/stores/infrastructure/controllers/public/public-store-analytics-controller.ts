2e62931aa5f16440ac74c17babf329c4a866b697
DQuintero15 <quinterod164@gmail.com>
2024-06-06 11:40:54 -0500

    feat: add saveClickedPayCart endpoint to PublicStoreAnalyticsController

--- src/modules/stores/infrastructure/controllers/public/public-store-analytics-controller.ts
+++ src/modules/stores/infrastructure/controllers/public/public-store-analytics-controller.ts
@@ -2,7 +2,10 @@ import { Body, Controller, Inject, Post, Req, Res } from "@nestjs/common"
 import { ApiTags } from "@nestjs/swagger"
 import { handlerHttpResponse } from "@shared/infrastructure/handlers"
 import { Request, Response } from "express"
-import { SaveStoreAnalyticCommand } from "src/modules/stores/application/command"
+import {
+	SaveClickedPayCartCommand,
+	SaveStoreAnalyticCommand
+} from "src/modules/stores/application/command"
 
 import { CreateStoreAnalyticsDto } from "../../../domain/dtos"
 import { StoresInfrastructureInjectionTokens } from "../../store-infrastructure-injection-tokens"
@@ -12,8 +15,40 @@ import { StoresInfrastructureInjectionTokens } from "../../store-infrastructure-
 export class PublicStoreAnalyticsController {
 	constructor(
 		@Inject(StoresInfrastructureInjectionTokens.SaveStoreAnalyticCommand)
-		private readonly saveStoreAnalyticsQuery: SaveStoreAnalyticCommand
+		private readonly saveStoreAnalyticsQuery: SaveStoreAnalyticCommand,
+
+		@Inject(StoresInfrastructureInjectionTokens.SaveClickedPayCartCommand)
+		private readonly saveClickedPayCartCommand: SaveClickedPayCartCommand
 	) {}
+
+	@Post("clicked-pay-cart")
+	saveClickedPayCart(
+		@Req() req: Request,
+		@Body() body: { ids: number[]; storeId: number },
+		@Res() res: Response
+	) {
+		const device = req.headers["user-agent"]
+
+		const data = { ...body, device }
+
+		try {
+			this.saveClickedPayCartCommand.execute(data.ids, data.storeId).then(() => {
+				return handlerHttpResponse(res, {
+					data: null,
+					message: "Clicked pay cart saved successfully",
+					statusCode: 201,
+					success: true
+				})
+			})
+		} catch (error) {
+			return handlerHttpResponse(res, {
+				data: null,
+				message: "Error saving clicked pay cart",
+				statusCode: 400,
+				success: false
+			})
+		}
+	}
 
 	@Post()
 	saveStoreAnalytics(
