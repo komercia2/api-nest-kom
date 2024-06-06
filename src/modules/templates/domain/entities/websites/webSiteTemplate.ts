253eda6d4828810177f51d00f9cd66f7a05409ee
DQuintero15 <quinterod164@gmail.com>
2024-06-06 17:54:31 -0500

    feat: add wapi-template.entity to templates domain entities

--- src/modules/templates/domain/entities/websites/webSiteTemplate.ts
+++ src/modules/templates/domain/entities/websites/webSiteTemplate.ts
@@ -1,4 +1,5 @@
 import { Template6Entity } from "../template6/template6"
 import { Template15 } from "../template15"
+import { WapiTemplateEntity } from "../wapi"
 
-export type WebSiteTemplate = Template15 | Template6Entity
+export type WebSiteTemplate = Template15 | Template6Entity | WapiTemplateEntity
