5768839720e17beb69ed7e356c02d587ecb83295
DQuintero15 <quinterod164@gmail.com>
2024-06-13 16:15:47 -0500

    chore: refactor wapi template entity and model naming conventions

--- src/modules/templates/domain/entities/wapi/wapi-template.entity.ts
+++ src/modules/templates/domain/entities/wapi/wapi-template.entity.ts
@@ -2,49 +2,49 @@ interface IWapiTemplateEntity {
 	_id: string
 	banner: string | null
 	descripcion: string | null
-	logoCuadrado: boolean | null
-	colorPrimario: string | null
-	colorSecundario: string | null
-	colorIcon: string | null
+	logo_cuadrado: boolean | null
+	color_primario: string | null
+	color_secundario: string | null
+	color_icon: string | null
 	tema: number | null
-	pagoOnline: boolean | null
-	mensajePrincipal: string | null
-	estiloProductos: number | null
-	estiloCategorias: number | null
+	pago_online: boolean | null
+	mensaje_principal: string | null
+	estilo_productos: number | null
+	estilo_categorias: number | null
 	watermark: boolean
-	stateSubcategorias: boolean
+	state_subcategorias: boolean
 }
 
 export class WapiTemplateEntity implements IWapiTemplateEntity {
 	_id: string
 	banner: string | null
 	descripcion: string | null
-	logoCuadrado: boolean | null
-	colorPrimario: string | null
-	colorSecundario: string | null
-	colorIcon: string | null
+	logo_cuadrado: boolean | null
+	color_primario: string | null
+	color_secundario: string | null
+	color_icon: string | null
 	tema: number | null
-	pagoOnline: boolean | null
-	mensajePrincipal: string | null
-	estiloProductos: number | null
-	estiloCategorias: number | null
+	pago_online: boolean | null
+	mensaje_principal: string | null
+	estilo_productos: number | null
+	estilo_categorias: number | null
 	watermark: boolean
-	stateSubcategorias: boolean
+	state_subcategorias: boolean
 
 	constructor(data: IWapiTemplateEntity) {
 		this._id = data._id
 		this.banner = data.banner
 		this.descripcion = data.descripcion
-		this.logoCuadrado = data.logoCuadrado
-		this.colorPrimario = data.colorPrimario
-		this.colorSecundario = data.colorSecundario
-		this.colorIcon = data.colorIcon
+		this.logo_cuadrado = data.logo_cuadrado
+		this.color_primario = data.color_primario
+		this.color_secundario = data.color_secundario
+		this.color_icon = data.color_icon
 		this.tema = data.tema
-		this.pagoOnline = data.pagoOnline
-		this.mensajePrincipal = data.mensajePrincipal
-		this.estiloProductos = data.estiloProductos
-		this.estiloCategorias = data.estiloCategorias
+		this.pago_online = data.pago_online
+		this.mensaje_principal = data.mensaje_principal
+		this.estilo_productos = data.estilo_productos
+		this.estilo_categorias = data.estilo_categorias
 		this.watermark = data.watermark
-		this.stateSubcategorias = data.stateSubcategorias
+		this.state_subcategorias = data.state_subcategorias
 	}
 }
