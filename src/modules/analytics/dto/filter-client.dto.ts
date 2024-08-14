10a26b6b5534449ef5a9bf64e648d61cbd16800c
Gustavo Moreno <gustavomorenojustplay@gmail.com>
2024-08-14 14:00:58 -0500

    feat: add sorting options to getClients endpoint in AnalyticsService

--- src/modules/analytics/dto/filter-client.dto.ts
+++ src/modules/analytics/dto/filter-client.dto.ts
@@ -1,7 +1,23 @@
-import { IsOptional, IsString } from "class-validator"
+import { Type } from "class-transformer"
+import { IsBoolean, IsOptional, IsString } from "class-validator"
 
 export class FilterClientDto {
 	@IsOptional()
 	@IsString()
 	readonly name?: string
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly sortByName?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly sortByTotal?: boolean
+
+	@IsOptional()
+	@IsBoolean()
+	@Type(() => Boolean)
+	readonly sortByTotalCarts?: boolean
 }
