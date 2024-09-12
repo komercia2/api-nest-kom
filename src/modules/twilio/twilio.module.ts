4a50d2315259edb568934ee6fe2eddeccde705bf
Daniel Quintero <quinterod164@gmail.com>
2024-09-12 16:57:48 -0500

    feat: add entities

--- src/modules/twilio/twilio.module.ts
+++ src/modules/twilio/twilio.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common"
+import { TypeOrmModule } from "@nestjs/typeorm"
+import { Users, UsersInfo } from "src/entities"
 
 import { TwilioController } from "./twilio.controller"
 import { TwilioService } from "./twilio.service"
 
 @Module({
+	imports: [TypeOrmModule.forFeature([UsersInfo, Users])],
 	controllers: [TwilioController],
 	providers: [TwilioService]
 })
