a5ea3e05f69d77f28a30c084b588d212e4abfec1
Daniel Quintero <quinterod164@gmail.com>
2024-09-12 15:51:11 -0500

    feat: add twilio module

---
+++ src/modules/twilio/twilio.controller.ts
@@ -0,0 +1,8 @@
+import { Controller } from "@nestjs/common"
+
+import { TwilioService } from "./twilio.service"
+
+@Controller("twilio")
+export class TwilioController {
+	constructor(private readonly twilioService: TwilioService) {}
+}
