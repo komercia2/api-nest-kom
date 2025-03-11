49852e184396e8e68e54c29963f8c2ed5b9927a2
Daniel Quintero <quinterod164@gmail.com>
2025-03-11 04:14:02 -0500

    feat(panel): add removeCountryCode method and update phone formatting in parsedClients

--- src/modules/panel/panel.service.ts
+++ src/modules/panel/panel.service.ts
@@ -140,7 +140,8 @@ export class PanelService {
 		const parsedClients = clients.map((client) => {
 			client.ultima_compra = new Date(client.ultima_compra).toISOString().split("T")[0]
 			client.usuario_uso_cupon = client.usuario_uso_cupon === "1" ? "SI" : "NO"
-			client.telefono = client.telefono ? client.telefono : "N/A"
+			client.telefono = client.telefono ? this.removeCountryCode(client.telefono) : "N/A"
+			client.identificacion = ` ${client.identificacion}`
 			client.compras_completadas = new Intl.NumberFormat("es-ES", {
 				style: "currency",
 				currency: this.mapCountrieCurrency(client.codigo_pais)
@@ -201,6 +202,17 @@ export class PanelService {
 		if (countrieCode.toLowerCase() === "pe") return "PEN"
 		if (countrieCode.toLowerCase() === "pan") return "USD"
 		return "COP"
+	}
+
+	removeCountryCode(phone: string) {
+		if (phone.startsWith("+57")) return phone.substring(3)
+		if (phone.startsWith("+52")) return phone.substring(3)
+		if (phone.startsWith("+54")) return phone.substring(3)
+		if (phone.startsWith("+56")) return phone.substring(3)
+		if (phone.startsWith("+1")) return phone.substring(2)
+		if (phone.startsWith("+51")) return phone.substring(3)
+		if (phone.startsWith("+507")) return phone.substring(4)
+		return phone
 	}
 
 	async deleteProductDeliveryStatus(cartID: number) {
