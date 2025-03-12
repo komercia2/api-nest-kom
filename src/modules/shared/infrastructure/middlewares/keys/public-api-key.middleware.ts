8d2459c73da289288521a3e4d246120082f24534
Daniel Quintero <quinterod164@gmail.com>
2025-03-11 19:14:11 -0500

    refactor(middleware): comment out public API key validation logic

--- src/modules/shared/infrastructure/middlewares/keys/public-api-key.middleware.ts
+++ src/modules/shared/infrastructure/middlewares/keys/public-api-key.middleware.ts
@@ -9,29 +9,29 @@ export class PublicApiKeyAuthMiddleware implements NestMiddleware {
 
 	async use(@Req() req: Request, res: Response, next: NextFunction) {
 		try {
-			const { komercia_public_routes_key } = req.headers
+			// const { komercia_public_routes_key } = req.headers
 
-			if (!komercia_public_routes_key) {
-				handlerHttpResponse(res, {
-					data: null,
-					message: "Public api key not found",
-					statusCode: HttpStatus.BAD_REQUEST,
-					success: false
-				})
-				return
-			}
+			// if (!komercia_public_routes_key) {
+			// 	handlerHttpResponse(res, {
+			// 		data: null,
+			// 		message: "Public api key not found",
+			// 		statusCode: HttpStatus.BAD_REQUEST,
+			// 		success: false
+			// 	})
+			// 	return
+			// }
 
-			const publicApiKeyFromEnv = this.configService.get<string>("KOMERCIA_PUBLIC_ROUTES_KEY")
+			// const publicApiKeyFromEnv = this.configService.get<string>("KOMERCIA_PUBLIC_ROUTES_KEY")
 
-			if (komercia_public_routes_key !== publicApiKeyFromEnv) {
-				handlerHttpResponse(res, {
-					data: null,
-					message: "Invalid public api key",
-					statusCode: HttpStatus.UNAUTHORIZED,
-					success: false
-				})
-				return
-			}
+			// if (komercia_public_routes_key !== publicApiKeyFromEnv) {
+			// 	handlerHttpResponse(res, {
+			// 		data: null,
+			// 		message: "Invalid public api key",
+			// 		statusCode: HttpStatus.UNAUTHORIZED,
+			// 		success: false
+			// 	})
+			// 	return
+			// }
 
 			next()
 		} catch (error) {
